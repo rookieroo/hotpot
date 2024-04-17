49be5bc3833e5104bfe1b42d3111f060a184f6c1
Din <13716066567@163.com>
2024-04-17 11:09:04 +0800

    fix: Link

--- src/components/home/home-tabs-list.tsx
+++ src/components/home/home-tabs-list.tsx
@@ -10,23 +10,21 @@ export function HomeTabsList({list}) {
     <>
       <div className="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {list.map(m =>
-          <Link href={m.href} legacyBehavior passHref>
-            <a>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    {m.code}
-                  </CardTitle>
-                  <m.icon className="h-5 w-5"/>
-                </CardHeader>
-                <CardContent>
-                  {/*<div className="text-2xl font-bold"></div>*/}
-                  <p className="text-xs text-muted-foreground">
-                    {m.description}
-                  </p>
-                </CardContent>
-              </Card>
-            </a>
+          <Link href={m.href} legacyBehavior>
+            <Card>
+              <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                <CardTitle className="text-sm font-medium">
+                  {m.code}
+                </CardTitle>
+                <m.icon className="h-5 w-5"/>
+              </CardHeader>
+              <CardContent>
+                {/*<div className="text-2xl font-bold"></div>*/}
+                <p className="text-xs text-muted-foreground">
+                  {m.description}
+                </p>
+              </CardContent>
+            </Card>
           </Link>
         )}
       </div>
